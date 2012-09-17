9653465fca64ef41c4fe6237614a7724b91c9a44
root <root@aether.(none)>
2012-09-17 00:57:43 -0400

    added initial stuff to the file doesn't do proper error handling, but can catch most things works in general

---
+++ matrixSolver.py
@@ -0,0 +1,32 @@
+from numpy import *
+import re
+
+line = '0'
+arr = []
+
+while(line != ''):
+	#gets input
+        line = raw_input('enter line\n')
+	
+	#if reached EOF marker
+        if(line == ''):
+                break
+
+	# fancy list comprehension.  Allows spaces and ; at the end of a line
+        arr.append([float(x) for x in line.split(';') if x != ''])
+
+
+# automagically splits the whole array into A and b
+A, b =  hsplit(array(arr), [len(arr[0]) - 1])
+
+
+# temporary debug lines
+print A
+print '\n\n'
+print b
+print '\n\n'
+
+x = dot(linalg.pinv(A), b)
+print x
+
+
