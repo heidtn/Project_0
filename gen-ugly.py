aab270fb55935038e8d6de041c390fab1161def6
Team 2 <hrb-team2@Microknoppix.(none)>
2012-09-17 16:27:38 +0000

    added generator files

---
+++ gen-ugly.py
@@ -0,0 +1,25 @@
+from numpy import *
+from pylab import *
+from sys import stdout, stderr
+
+N = randint(250)
+X = randn(N,N+1)
+
+U,s,V = svd(X[:,:-1])
+s **= 4
+X[:,:-1] = dot(U*s,V)
+
+for r,row in enumerate(X):
+  for c,val in enumerate(row):
+    fmt = "%%%d.%d%s" % ( randint(3,15), randint(0,8), "efg"[randint(3)] )
+    stdout.write( fmt % val )
+    if randint(10)==0:
+      delim = "\t"*int(4*rand()**3)+";"+" "*int(10*rand()**3)    
+    else:
+      delim = " "*int(10*rand()**3)+";"+" "*int(10*rand()**3)    
+    if c==N:
+      stdout.write( "\n" if randint(5)>0 else delim+"\n" )
+    else:
+      stdout.write( delim )
+
+stderr.write("valid N=%d\n" % N)
