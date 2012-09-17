44bfd839816f9b32b8b5340cfb482248e2f41109
Matthew Doyle <msdoyle@umich.edu>
2012-09-17 15:38:50 -0400

    Second timing test, using timeit.
    Second timing test, using timeit. Doesn't work yet.

---
+++ timing_test_02_02.py
@@ -0,0 +1,49 @@
+import sys
+
+#timeit module import for performance testing
+import timeit
+
+
+def main():
+  
+  #take arg after filename at runtime
+  x = int(sys.argv[1])
+  
+  #set start time
+  #starttime = time.time()
+  
+  #run repeater recursive fn to test (997 is the max for my machine, for whatever reason)-MD
+  repeater(x)
+  
+  #mark end time
+  #endtime = time.time()
+  
+  #calc elapsed and put into minute form, just because
+  #elapsedtime = endtime - starttime
+  #mintime = elapsedtime/60
+  #print time elapsed
+  #print "Time elapsed:", elapsedtime, "seconds, which is:", mintime, "minutes"
+  
+
+
+def repeater(x):
+  print x
+  x = x - 1
+  if x >= 0:
+    return repeater(x)
+  else:
+    return
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
+
+#if __name__ == '__main__':
+    #from timeit import Timer
+    #t = Timer("main()", "from __main__ import main")
+    #print t.timeit()
+
+  
+#timing code from:http://web.archive.org/web/20100825233709/http://www.techarticles.zeromu.net/programming/keeping-track-of-elapsed-time-in-python/
+  
+  
