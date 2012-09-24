42baef7b66e8e5ba67f8e69e4b3a33d121f3ebb9
calebsyoon <yoon.caleb@yahoo.com>
2012-09-24 01:39:23 -0300

    Update matrixSolver.py

--- matrixSolver.py
+++ matrixSolver.py
@@ -1,39 +1,37 @@
 from numpy import *
 import re
 class IOException(Exception):
-	pass
+        pass
 class InputFormatError(IOException):
-	def __init__ (self, expr, msg):
-		self.expr = expr
-		self.msg = msg
-		print "Input Format Error, Bad input"
+        def __init__ (self, line, str):
+                print "Input Format Error, Bad input"
 
 line = '0'
 arr = []
 
 while(line != ''):
-	#gets input
-	try:
-        	line = raw_input()
-	except EOFError:
-		break
-		
-	#if reached EOF marker
+        #gets input
+        try:
+                line = raw_input()
+        except EOFError:
+                break
+
+        #if reached EOF marker
         if not line: break
-	line = line.strip()
-	
-	# fancy list comprehension.  Allows spaces and ; at the end of a line
-	try:
-		arr.append([float(x) for x in line.split(';') if x != ''])
-	except InputFormatError:
-		print "Invalid input cannot be parsed"
+        line = line.strip()
+
+        # fancy list comprehension.  Allows spaces and ; at the end of a line
+        try:
+                arr.append([float(x) for x in line.split(';') if x != ''])
+        except ValueError:
+                raise InputFormatError(line, "Input Format Error: Bad Input")
+                sys.exit()
 
 # automagically splits the whole array into A and b
 A, b =  hsplit(array(arr), [len(arr[0]) - 1])
 
 
-# temporary debug lines 
+# temporary debug lines
 x = dot(linalg.pinv(A), b)
 for i in x:
-	print str(i).format("%6.3e")
-
+        print str(i).format("%6.3e")
